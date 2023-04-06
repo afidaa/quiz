64ebaf9dfdcaedef5bb171f0b020a5e46e1079e2
Afida Valieva <afidaav@code.edu.az>
2023-04-06 12:50:15 +0400

    task

---
+++ TASK/assets/script.js
@@ -0,0 +1,10 @@
+let i = document.getElementById(inp)
+let b = document.getElementById(btn)
+
+function send(){ 
+if(input.value == 4)
+    console.log(kod);
+}
+b.addEventListener("click", send)
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
